48fc6469a2d14f0ec0424732c36c4baa20834388
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-02-17 01:08:55 -0800

    Add timing snippet.

--- uf_set/UnionFindSet.py
+++ uf_set/UnionFindSet.py
@@ -111,79 +111,109 @@ class UnionFindSet :
         return self.get_set_from_id(self.elem_ids[elem])
 
 if __name__ == '__main__' :
-    # normal test cases
-    L = {'a', 'b', 'c', 'd', 'e', 'f', 'g'}
-    U = UnionFindSet(L)
-    U.union('a', 'g')
-    U.union('g', 'c')
-    U.union('a', 'e')
-    assert U.group('a') == U.group('g') == U.group('c')
-    assert U.group('f') != U.group('d')
-    assert U.group('a') != U.group('d')
-    U.union('b', 'g')
-    assert set(U.get_set('g')) == set(['a', 'c', 'g', 'b', 'e'])
+    from random import randint
+    import matplotlib.pyplot as plt
+    from timeit import timeit
+    
+    def normal_test_cases() :
+        '''
+        Easy test cases for the union-find disjoint-set.
+        '''
+        L = {'a', 'b', 'c', 'd', 'e', 'f', 'g'}
+        U = UnionFindSet(L)
+        U.union('a', 'g')
+        U.union('g', 'c')
+        U.union('a', 'e')
+        assert U.group('a') == U.group('g') == U.group('c')
+        assert U.group('f') != U.group('d')
+        assert U.group('a') != U.group('d')
+        U.union('b', 'g')
+        assert set(U.get_set('g')) == set(['a', 'c', 'g', 'b', 'e'])
 
     # test case generator
     # compare the opeerations with set class provided
     # by standard python library
-    test_cases = 1
-    test_size = 1000
-    query_size = 300
-    from random import randint
-    for t in range(1) :
-        x = UnionFindSet([ i for i in range(test_size) ])
-        y = []
-        for i in range(query_size) :
-            p1 = randint(0, test_size-2)
-            p2 = randint(p1, test_size-1)
-            x.union(p1, p2)
-            print('Union:', p1, p2)
-            # union and update y
-            lo1 = -1
-            lo2 = -1
+    def generated_test_cases(test_cases, test_size, query_size) :
+        '''
+        Generated test cases for the set.
+        '''
+        for t in range(test_cases) :
+            x = UnionFindSet([ i for i in range(test_size) ])
+            y = []
+            for i in range(query_size) :
+                p1 = randint(0, test_size-2)
+                p2 = randint(p1, test_size-1)
+                x.union(p1, p2)
+                print('Union:', p1, p2)
+                # union and update y
+                lo1 = -1
+                lo2 = -1
+                for i in range(len(y)) :
+                    if p1 in y[i] :
+                        lo1 = i
+                    if p2 in y[i] :
+                        lo2 = i
+                if lo1 == -1 and lo2 == -1 :
+                    # print(y, '<--', set([p1, p2]))
+                    y += [set([p1, p2])]
+                if lo1 == -1 and lo2 != -1 :
+                    # print(y, y[lo2], '<--', set([p1]))
+                    y[lo2] |= set([p1])
+                if lo1 != -1 and lo2 == -1 :
+                    # print(y, y[lo1], '<--', set([p2]))
+                    y[lo1] |= set([p2])
+                if lo1 != -1 and lo2 != -1 and lo1 != lo2 :
+                    # print(y, y[lo1], '<--', y[lo2], '(removed)')
+                    y[lo1] |= y[lo2]
+                    y.pop(lo2)
             for i in range(len(y)) :
-                if p1 in y[i] :
-                    lo1 = i
-                if p2 in y[i] :
-                    lo2 = i
-            if lo1 == -1 and lo2 == -1 :
-                # print(y, '<--', set([p1, p2]))
-                y += [set([p1, p2])]
-            if lo1 == -1 and lo2 != -1 :
-                # print(y, y[lo2], '<--', set([p1]))
-                y[lo2] |= set([p1])
-            if lo1 != -1 and lo2 == -1 :
-                # print(y, y[lo1], '<--', set([p2]))
-                y[lo1] |= set([p2])
-            if lo1 != -1 and lo2 != -1 and lo1 != lo2 :
-                # print(y, y[lo1], '<--', y[lo2], '(removed)')
-                y[lo1] |= y[lo2]
-                y.pop(lo2)
-        for i in range(len(y)) :
-            print('Sets %d in y:' % (i+1), y[i])
-        
-        # Now check whether things are in the same
-        # set
-        for i in range(query_size) :
-            p1 = randint(0, test_size-2)
-            p2 = randint(p1, test_size-1)
-            print('Test %d:' % (i+1), p1, p2, end=' ')
-            lo1 = -1
-            lo2 = -1
-
-            for i in range(len(y)) :
-                if p1 in y[i] :
-                    lo1 = i
-                if p2 in y[i] :
-                    lo2 = i
-            if p1 == p2 or (lo1 != -1 and lo1 == lo2) :
-                samegroup = True
-                print('samegroup')
-            else :
-                samegroup = False
-                print('not samegroup')
+                print('Sets %d in y:' % (i+1), y[i])
             
-            assert (x.group(p1) == x.group(p2)) == samegroup
+            # Now check whether things are in the same
+            # set
+            for i in range(query_size) :
+                p1 = randint(0, test_size-2)
+                p2 = randint(p1, test_size-1)
+                print('Test %d:' % (i+1), p1, p2, end=' ')
+                lo1 = -1
+                lo2 = -1
+
+                for i in range(len(y)) :
+                    if p1 in y[i] :
+                        lo1 = i
+                    if p2 in y[i] :
+                        lo2 = i
+                if p1 == p2 or (lo1 != -1 and lo1 == lo2) :
+                    samegroup = True
+                    print('samegroup')
+                else :
+                    samegroup = False
+                    print('not samegroup')
+                
+                assert (x.group(p1) == x.group(p2)) == samegroup
+        
+        def timing_snippet(test_size, query_size) :
+            '''
+            Timing snippet fot the timit function.
+            '''
+            x = UnionFindSet([ i for i in range(test_size) ])
+            y = []
+            for i in range(query_size) :
+                p1 = randint(0, test_size-2)
+                p2 = randint(p1, test_size-1)
+                x.union(p1, p2)
+            for i in range(query_size) :
+                p = randint(0, test_size-1)
+                x.find(p)
+            
+        
+        normal_test_cases()
+        generated_test_cases(1, 1000, 600)
+
+        def plot_ufset() :
+
+    
+
         
         
 
